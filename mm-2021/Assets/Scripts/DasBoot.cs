018faee32d7bfe69cbc56ff31bc44e753a4f9547
Sloan Kelly <sloan@codehoose.com>
2021-02-06 13:07:07 -0500

    Game over screen

---
+++ mm-2021/Assets/Scripts/DasBoot.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class DasBoot : MonoBehaviour
+{
+    IEnumerator Start()
+    {
+        int x = (int)transform.position.x;
+        int y = (int)transform.position.y;
+
+        while (y > 16)
+        {
+            y = y - 1;
+            transform.position = new Vector3(x, y);
+
+            yield return null;
+        }
+    }
+}
